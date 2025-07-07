bfac3c218382c5d9aeb344f88e432eb0f8c49c6f
Murilo Melo <Murilo.Melo@wta.local>
2025-07-07 12:55:54 -0300

    Correção do payload

--- message_processor.py
+++ message_processor.py
@@ -53,28 +53,18 @@ class MessageProcessor:
         # Mapeamento explícito para garantir nomes idênticos ao banco
         def normalize_keys(d):
             mapping = {
-                # Mapeamento para nomes exatos do banco
-                "battpercent": "batt_percent",
-                "beacon_serial": "beacon_serial",
-                "crc": "crc",
-                "epochtime_b": "epochtime_b",
-                "epochtime_g": "epochtime_g",
-                "fw_version_build": "fw_version_build",
-                "fw_version_major": "fw_version_major",
-                "fw_version_minor": "fw_version_minor",
-                "fw_version_patch": "fw_version_patch",
-                "fw_version_prefix": "fw_version_prefix",
-                "id": "id",
-                "ln2accdataavailable": "ln2_acc_data_available",
-                "ln2anglestatus": "ln2_angle_status",
-                "ln2batterystatus": "ln2_battery_status",
-                "ln2foamstatus": "ln2_foam_status",
-                "ln2generalstatus": "ln2_general_status",
-                "ln2levelstatus": "ln2_level_status",
-                "ln2txcausestatus": "ln2_tx_cause_status",
-                "original_payload": "original_payload",
-                "package_id": "package_id",
+                "topic": "topic",
+                "start_flag": "start_flag",
                 "package_type": "package_type",
+                "beacon_serial": "beacon_serial",
+                # "vccbat": "vccbat",
+                "epochtime_b": "epochtime_b",
+                "epochtime_btx": "epochtime_btx",
+                "epochtime_g": "epochtime_g",
+                "crc": "crc",
+                "rssi": "rssi",
+                "package_id": "package_id",
+                "tempa": "tempa",
                 "r1": "r1",
                 "r2": "r2",
                 "r3": "r3",
@@ -86,19 +76,32 @@ class MessageProcessor:
                 "r9": "r9",
                 "r10": "r10",
                 "r11": "r11",
-                "rssi": "rssi",
                 "sensor_data_def_id": "sensor_data_def_id",
-                "start_flag": "start_flag",
-                "statusosccnt_lsb": "status_osc_cnt_lsb",
-                "statusosccnt_msb": "status_osc_cnt_msb",
-                "temp2": "temp2",
-                "topic": "topic",
-                "vccbat": "vccbat",
+                "fw_version_prefix": "fw_version_prefix",
+                "fw_version_major": "fw_version_major",
+                "fw_version_minor": "fw_version_minor",
+                "fw_version_patch": "fw_version_patch",
+                "fw_version_build": "fw_version_build",
+                "acc_mode_full": "acc_mode_full",
+                "ton_toff": "ton_toff",
+                "ln2_level_status": "ln2_level_status",
+                "ln2_angle_status": "ln2_angle_status",
+                "ln2_battery_status": "ln2_battery_status",
+                "batt_percent": "batt_percent",
+                "ln2_foam_status": "ln2_foam_status",
+                "ln2_general_status": "ln2_general_status",
+                "status_osc_cnt": "status_osc_cnt",
+                "ln2_acc_data_available": "ln2_acc_data_available",
+                "ln2_tx_cause_status": "ln2_tx_cause_status",
+                "temp_pt100": "temp_pt100",
+                "temp_ambient": "temp_ambient",
+                "angle_to_horizontal": "angle_to_horizontal",
+                "vbat_mv": "vbat_mv",
+                "original_payload": "original_payload",
             }
             normalized = {}
             for k, v in d.items():
                 key = k.lower().replace("-", "_")
-                # Permite mapear chaves em maiúsculas/minúsculas
                 if key in mapping:
                     normalized[mapping[key]] = v
                 elif key.upper() in mapping:
@@ -112,13 +115,13 @@ class MessageProcessor:
             return
         sql = (
             "INSERT INTO mqtt_messages ("
-            "topic, start_flag, package_type, beacon_serial, vccbat, epochtime_b, epochtime_g, crc, package_id, temp2, "
+            "topic, start_flag, package_type, beacon_serial, epochtime_b, epochtime_btx, epochtime_g, crc, package_id, tempa, "
             "r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, "
             "sensor_data_def_id, fw_version_prefix, fw_version_major, fw_version_minor, fw_version_patch, fw_version_build, "
-            "ln2_level_status, ln2_angle_status, ln2_battery_status, batt_percent, ln2_foam_status, ln2_general_status, "
-            "status_osc_cnt_msb, status_osc_cnt_lsb, ln2_acc_data_available, ln2_tx_cause_status, rssi, original_payload"
+            "acc_mode_full, ton_toff, ln2_level_status, ln2_angle_status, ln2_battery_status, batt_percent, ln2_foam_status, ln2_general_status, "
+            "status_osc_cnt, ln2_acc_data_available, ln2_tx_cause_status, temp_pt100, temp_ambient, angle_to_horizontal, vbat_mv, rssi, original_payload"
             ") VALUES ("
-            + ",".join(["%s"] * 39) + ")"
+            + ",".join(["%s"] * 44) + ")"
         )
         def safe_float(val):
             try:
@@ -139,41 +142,47 @@ class MessageProcessor:
             message_dict.get("start_flag"),  # 2
             message_dict.get("package_type"),  # 3
             message_dict.get("beacon_serial"),  # 4
-            safe_float(message_dict.get("vccbat")),  # 5
+            # safe_float(message_dict.get("vccbat")),  # 5
             message_dict.get("epochtime_b"),  # 6
-            message_dict.get("epochtime_g"),  # 7
-            message_dict.get("crc"),  # 8
-            safe_int(message_dict.get("package_id")),  # 9
-            safe_float(message_dict.get("temp2")),  # 10
-            message_dict.get("r1"),  # 11
-            message_dict.get("r2"),  # 12
-            message_dict.get("r3"),  # 13
-            message_dict.get("r4"),  # 14
-            message_dict.get("r5"),  # 15
-            message_dict.get("r6"),  # 16
-            message_dict.get("r7"),  # 17
-            message_dict.get("r8"),  # 18
-            message_dict.get("r9"),  # 19
-            message_dict.get("r10"),  # 20
-            message_dict.get("r11"),  # 21
-            message_dict.get("sensor_data_def_id"),  # 22
-            message_dict.get("fw_version_prefix"),  # 23
-            message_dict.get("fw_version_major"),  # 24
-            message_dict.get("fw_version_minor"),  # 25
-            message_dict.get("fw_version_patch"),  # 26
-            message_dict.get("fw_version_build"),  # 27
-            message_dict.get("ln2_level_status"),  # 28
-            message_dict.get("ln2_angle_status"),  # 29
-            message_dict.get("ln2_battery_status"),  # 30
-            message_dict.get("batt_percent"),  # 31
-            message_dict.get("ln2_foam_status"),  # 32
-            message_dict.get("ln2_general_status"),  # 33
-            message_dict.get("status_osc_cnt_msb"),  # 34
-            message_dict.get("status_osc_cnt_lsb"),  # 35
-            message_dict.get("ln2_acc_data_available"),  # 36
-            message_dict.get("ln2_tx_cause_status"),  # 37
-            safe_int(message_dict.get("rssi")),  # 38
-            message_dict.get("original_payload"),  # 39 REMOVIDO!
+            message_dict.get("epochtime_btx"),  # 7
+            message_dict.get("epochtime_g"),  # 8
+            message_dict.get("crc"),  # 9
+            safe_int(message_dict.get("package_id")),  # 10
+            safe_float(message_dict.get("tempa")),  # 11
+            message_dict.get("r1"),  # 12
+            message_dict.get("r2"),  # 13
+            message_dict.get("r3"),  # 14
+            message_dict.get("r4"),  # 15
+            message_dict.get("r5"),  # 16
+            message_dict.get("r6"),  # 17
+            message_dict.get("r7"),  # 18
+            message_dict.get("r8"),  # 19
+            message_dict.get("r9"),  # 20
+            message_dict.get("r10"),  # 21
+            message_dict.get("r11"),  # 22
+            message_dict.get("sensor_data_def_id"),  # 23
+            message_dict.get("fw_version_prefix"),  # 24
+            message_dict.get("fw_version_major"),  # 25
+            message_dict.get("fw_version_minor"),  # 26
+            message_dict.get("fw_version_patch"),  # 27
+            message_dict.get("fw_version_build"),  # 28
+            message_dict.get("acc_mode_full"),  # 29
+            message_dict.get("ton_toff"),  # 30
+            message_dict.get("ln2_level_status"),  # 31
+            message_dict.get("ln2_angle_status"),  # 32
+            message_dict.get("ln2_battery_status"),  # 33
+            message_dict.get("batt_percent"),  # 34
+            message_dict.get("ln2_foam_status"),  # 35
+            message_dict.get("ln2_general_status"),  # 36
+            message_dict.get("status_osc_cnt"),  # 37
+            message_dict.get("ln2_acc_data_available"),  # 38
+            message_dict.get("ln2_tx_cause_status"),  # 39
+            safe_float(message_dict.get("temp_pt100")),  # 40
+            safe_float(message_dict.get("temp_ambient")),  # 41
+            safe_float(message_dict.get("angle_to_horizontal")),  # 42
+            safe_float(message_dict.get("vbat_mv")),  # 43
+            safe_int(message_dict.get("rssi")),
+            message_dict.get("original_payload"),  # 44
         )
         try:
             # Debug: conferir número de argumentos e tipos
@@ -196,84 +205,42 @@ class MessageProcessor:
 
     def _load_schema(self) -> list[dict]:
         schema = [
-            {"name": "start_flag", "start_idx": 0, "end_idx": 2},
-            {"name": "package_type", "start_idx": 2, "end_idx": 4},
-            {"name": "beacon_serial", "start_idx": 4, "end_idx": 16},
-            {"name": "vccBat", "start_idx": 16, "end_idx": 20},
-            {"name": "epochtime_b", "start_idx": 20, "end_idx": 28},
-            {"name": "epochtime_g", "start_idx": 28, "end_idx": 36},
-            {"name": "crc", "start_idx": 36, "end_idx": 38},
-            {"name": "package_id", "start_idx": 38, "end_idx": 42},
-            {"name": "temp2", "start_idx": 42, "end_idx": 46},
-
-            # Reserved fields
-            *[{"name": f"r{i}", "start_idx": 46 + (i - 1) * 2, "end_idx": 46 + i * 2} for i in range(1, 12)],
-            # {"name": "r1", "start_idx": 46, "end_idx": 48},
-
-            {"name": "SENSOR_DATA_DEF_ID", "start_idx": 50, "end_idx": 52},
-            {"name": "FW_VERSION_PREFIX", "start_idx": 52, "end_idx": 54},
-
-            {"name": "FW_VERSION_MAJOR", "start_idx": 54, "end_idx": 56},
-            {"name": "FW_VERSION_MINOR", "start_idx": 56, "end_idx": 58},
-            {"name": "FW_VERSION_PATCH", "start_idx": 58, "end_idx": 60},
-            {"name": "FW_VERSION_BUILD", "start_idx": 60, "end_idx": 62},
-            # {"name": "BEHAVIOR_OUT", "start_idx": 56, "end_idx": 58},
-            # *[{"name": f"r{i}", "start_idx": 46 + (i - 1) * 4, "end_idx": 46 + i * 4} for i in range(7, 12)]
-
-            {"name": "Ln2LevelStatus", "start_idx": 90, "end_idx": 92},
-            {"name": "LN2AngleStatus", "start_idx": 92, "end_idx": 94},
-            {"name": "LN2BatteryStatus", "start_idx": 94, "end_idx": 96},
-            {"name": "BattPercent", "start_idx": 96, "end_idx": 98},
-            {"name": "LN2FoamStatus", "start_idx": 98, "end_idx": 100},
-            {"name": "LN2GeneralStatus", "start_idx": 100, "end_idx": 102},
-            {"name": "StatusOscCnt_MSB", "start_idx": 102, "end_idx": 104},
-            {"name": "StatusOscCnt_LSB", "start_idx": 104, "end_idx": 106},
-            {"name": "LN2AccDataAvailable", "start_idx": 106, "end_idx": 108},
-            {"name": "LN2TxCauseStatus", "start_idx": 108, "end_idx": 110},
-            # Adicione outros campos conforme necessário
+            {"name": "start_flag", "start_idx": 0, "end_idx": 2},            # [0]
+            {"name": "package_type", "start_idx": 2, "end_idx": 4},          # [1]
+            {"name": "beacon_serial", "start_idx": 4, "end_idx": 16},        # [2-7]
+            # {"name": "vccbat", "start_idx": 16, "end_idx": 20},              # [8-9]
+            {"name": "epochtime_b", "start_idx": 20, "end_idx": 28},         # [10-13]
+            {"name": "epochtime_btx", "start_idx": 74, "end_idx": 82},       # [37-40] Beacon Tx Time
+            {"name": "epochtime_g", "start_idx": 28, "end_idx": 36},         # [14-17]
+            {"name": "crc", "start_idx": 36, "end_idx": 38},                 # [18]
+            {"name": "package_id", "start_idx": 38, "end_idx": 42},          # [19-20]
+            {"name": "tempa", "start_idx": 42, "end_idx": 46},               # [21-22]
+            # r1 a r11 intactos
+            *[{"name": f"r{i}", "start_idx": 46 + (i - 1) * 4, "end_idx": 46 + (i - 1) * 4 + 4} for i in range(1, 12)],
+            # Campos específicos mapeados nos mesmos offsets
+            {"name": "sensor_data_def_id", "start_idx": 50, "end_idx": 52},  # [25]
+            {"name": "fw_version_prefix", "start_idx": 52, "end_idx": 54},   # [26]
+            {"name": "fw_version_major", "start_idx": 54, "end_idx": 56},    # [27]
+            {"name": "fw_version_minor", "start_idx": 56, "end_idx": 58},    # [28]
+            {"name": "fw_version_patch", "start_idx": 58, "end_idx": 60},    # [29]
+            {"name": "fw_version_build", "start_idx": 60, "end_idx": 62},    # [30]
+            {"name": "acc_mode_full", "start_idx": 82, "end_idx": 86},       # [41-42]
+            {"name": "ton_toff", "start_idx": 86, "end_idx": 90},             # [43-44]
+            {"name": "ln2_level_status", "start_idx": 90, "end_idx": 92},      # [45]
+            {"name": "ln2_angle_status", "start_idx": 92, "end_idx": 94},      # [46]
+            {"name": "ln2_battery_status", "start_idx": 94, "end_idx": 96},    # [47]
+            {"name": "batt_percent", "start_idx": 96, "end_idx": 98},         # [48]
+            {"name": "ln2_foam_status", "start_idx": 98, "end_idx": 100},      # [49]
+            {"name": "ln2_general_status", "start_idx": 100, "end_idx": 102},  # [50]
+            {"name": "status_osc_cnt", "start_idx": 102, "end_idx": 106},      # [51-52]
+            {"name": "ln2_acc_data_available", "start_idx": 106, "end_idx": 108},# [53]
+            {"name": "ln2_tx_cause_status", "start_idx": 108, "end_idx": 110},  # [54]
+            {"name": "temp_pt100", "start_idx": 112, "end_idx": 116},         # [56-57]
+            {"name": "temp_ambient", "start_idx": 116, "end_idx": 120},       # [58-59]
+            {"name": "angle_to_horizontal", "start_idx": 120, "end_idx": 124}, # [60-61]
+            {"name": "vbat_mv", "start_idx": 124, "end_idx": 128},           # [62-63]
+            {"name": "rssi", "start_idx": 486, "end_idx": 488},                # [62-63]
         ]
-
-        # Sensor data
-        start_idx = 90
-        sensor_iterations = 0 #4
-        idx = start_idx
-
-        for i in range(sensor_iterations):
-            # In each iteration, we have 3 sets of x, y, z
-            for j in range(1, 16):
-                schema.append({
-                    "name": f"x{15 * i + j}",
-                    "start_idx": idx,
-                    "end_idx": idx + 2
-                })
-                idx += 2
-
-                schema.append({
-                    "name": f"y{15 * i + j}",
-                    "start_idx": idx,
-                    "end_idx": idx + 2
-                })
-                idx += 2
-
-                schema.append({
-                    "name": f"z{15 * i + j}",
-                    "start_idx": idx,
-                    "end_idx": idx + 2
-                })
-                idx += 2
-
-            # After each block of 9 (x, y, z) fields (3 × 3), we have a temp reading
-            schema.append({
-                "name": f"--- temp{i + 1} ---",
-                "start_idx": idx,
-                "end_idx": idx + 4
-            })
-            idx += 4
-
-
-        # RSSI
-        schema.append({"name": "rssi", "start_idx": 486, "end_idx": 488})
-        
         return schema
 
     
@@ -310,18 +277,18 @@ class MessageProcessor:
         parsed_hex = {}
 
         for k, v in values.items():
-            if k in ['tempPT100', 'tempAmbient']:
+            if k in ['temp_pt100', 'temp_ambient']:
                 # Interpretar como int16_t em centésimos de grau Celsius
                 try:
                     raw_bytes = bytes.fromhex(v)
                     parsed_hex[k] = struct.unpack('>h', raw_bytes)[0] / 100
                 except Exception:
                     parsed_hex[k] = v  # fallback para depuração se falhar
-            elif k == 'Vbat_mV':
+            elif k == 'vbat_mv':
                 raw_bytes = bytes.fromhex(v)
                 vbat = struct.unpack('>H', raw_bytes)[0]
                 parsed_hex[k] = vbat
-            elif k == 'AngleToHorizontal':
+            elif k == 'angle_to_horizontal':
                 raw_bytes = bytes.fromhex(v)
                 angle = struct.unpack('>H', raw_bytes)[0]
                 parsed_hex[k] = angle
@@ -332,9 +299,9 @@ class MessageProcessor:
                 parsed_hex[k] = datetime.fromtimestamp(parsed_hex[k]).strftime("%Y-%m-%d %H-%M-%S.%f")[:-3]
             elif 'package_id' in k:
                 parsed_hex[k] = int(v, 16)
-            elif 'vccBat' in k:
+            elif 'vccbat' in k:
                 parsed_hex[k] = int(v, 16) / 1000
-            elif 'temp' in k:
+            elif 'tempa' in k:
                 parsed_hex[k] = int(v, 16) / 100
             elif k.endswith('Status'):
                 try:
